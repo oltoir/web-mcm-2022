294b90738a86f92bc4f56567b9aecd5cb8f8f062
oltoir <altair.zhanseitov@gmail.com>
2022-12-27 19:02:06 +0600

    feat: main page slider

---
+++ src/components/MainSwiper/index.tsx
@@ -0,0 +1,31 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Navigation, Pagination } from 'swiper';
+import 'assets/styles/swiper.css';
+
+type Props = {
+	slides: {
+		image: string;
+		href: string;
+	}[];
+};
+export function MainSwiper(props: Props) {
+	const { slides } = props;
+
+	return (
+		<Swiper
+			navigation={true}
+			modules={[Navigation, Pagination]}
+			className="mySwiper"
+			loop={true}
+		>
+			{slides.map((slide) => (
+				<SwiperSlide>
+					<a href={slide.href}>
+						<img src={slide.image} alt={slide.href} />
+					</a>
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
+}
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,30 @@
+import { MainSwiper } from 'components/MainSwiper';
+
+const slides = [
+	{
+		image:
+			'https://jusan.kz/file-server/filename?dir=banner&filename=1440x374_РУС-min.webp',
+		href: '/',
+	},
+	{
+		image:
+			'https://jusan.kz/file-server/filename?dir=slider&filename=business-car-desktop-ru.webp',
+		href: '/',
+	},
+	{
+		image:
+			'https://jusan.kz/file-server/filename?dir=slider&filename=BARISTO-1440x374-ru-min.webp',
+		href: '/',
+	},
+];
 function MainPage() {
-	return <div>Main Page</div>;
+	return (
+		<section className="h-full bg-gray-100">
+			<div className="pb-12 pt-9 mx-auto" style={{ maxWidth: '1200px' }}>
+				<MainSwiper slides={slides} />
+			</div>
+		</section>
+	);
 }
 
 export default MainPage;
